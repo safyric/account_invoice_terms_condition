83084f321dfeffb897f9a49a1605ce6c4dcba400
jchanme <39323414+jchanme@users.noreply.github.com>
2021-11-30 20:44:24 +0800

    Update account_invoice.py

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    comment = fields.Html('Additional Information', readonly=True, states={'draft': [('readonly', False)]})
+    comment = fields.Html('Additional Information')
