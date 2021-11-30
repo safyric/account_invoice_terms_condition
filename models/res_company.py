82413413872f603437975ae66ee72f65421c2d79
jchanme <39323414+jchanme@users.noreply.github.com>
2021-11-30 20:35:59 +0800

    Update res_company.py

--- models/res_company.py
+++ models/res_company.py
@@ -5,6 +5,6 @@ class ResCompany(models.Model):
 
     _inherit = 'res.company'
 
-    invoice_comment = fields.Html(
+    comment = fields.Html(
         string='Invoice Default Terms and Conditions',
         translate=True)
