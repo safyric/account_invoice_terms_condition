1be9b1b34bb74bb54652eae1e085158399929547
jchanme <39323414+jchanme@users.noreply.github.com>
2021-11-30 21:50:27 +0800

    Update account_invoice.py

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -1,8 +1,12 @@
-from odoo import fields, models
+from odoo import api, fields, models, _
 
 
 class AccountInvoice(models.Model):
 
     _inherit = "account.invoice"
 
-    comment = fields.Html(string='Additional Information')
+    comment = fields.Html(string='Additional Information', default=_default_comment)
+
+    @api.model
+    def _default_comment(self):
+        return self.env['ir.config_parameter'].sudo().get_param('invoice.use_invoice_comment') and self.env.user.company_id.invoice_comment or ''
