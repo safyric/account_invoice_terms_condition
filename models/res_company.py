6b31f45b975e51425e69574c6fc1cc6df067aaa9
jchanme <39323414+jchanme@users.noreply.github.com>
2021-11-30 15:06:34 +0800

    Create res_company.py

---
+++ models/res_company.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+
+    _inherit = 'res.company'
+
+    invoice_comment = fields.Html(
+        string='Invoice Default Terms and Conditions',
+        translate=True)
