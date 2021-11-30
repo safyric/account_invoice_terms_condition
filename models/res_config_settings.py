08b76afc32d9a49be3cf2b5e279514ebe51f9444
jchanme <39323414+jchanme@users.noreply.github.com>
2021-11-30 15:05:54 +0800

    Create res_config_settings.py

---
+++ models/res_config_settings.py
@@ -0,0 +1,15 @@
+from odoo import fields, models
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = 'res.config.settings'
+
+    default_comment = fields.Html(
+        related='company_id.invoice_comment',
+        string="Invoice Terms & Conditions",
+        default_model="account.invoice",
+        readonly=False)
+
+    use_invoice_comment = fields.Boolean(
+        string='Use Invoice Default Terms & Conditions',
+        config_parameter='account.use_invoice_comment')
