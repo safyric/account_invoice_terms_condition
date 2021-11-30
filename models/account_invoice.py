169a68556e87e1adac7914c419950c8fd62c050e
jchanme <39323414+jchanme@users.noreply.github.com>
2021-11-30 15:09:12 +0800

    Create account_invoice.py

---
+++ models/account_invoice.py
@@ -0,0 +1,6 @@
+from odoo import fields, models
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    comment = fields.Html('Additional Information', readonly=True, states={'draft': [('readonly', False)]})
